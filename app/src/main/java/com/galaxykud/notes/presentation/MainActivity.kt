d83a93c868e6496c0ec4375bda50fe7a2e477213
galaxykud <galaxykud@gmail.com>
2025-10-05 22:49:01 +0700

    add jetpack compose navigation (#1)

--- app/src/main/java/com/galaxykud/notes/presentation/MainActivity.kt
+++ app/src/main/java/com/galaxykud/notes/presentation/MainActivity.kt
@@ -1,19 +1,11 @@
 package com.galaxykud.notes.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.galaxykud.notes.presentation.screens.editing.EditNoteScreen
-import com.galaxykud.notes.presentation.screens.notes.NotesScreen
+import com.galaxykud.notes.presentation.navigation.CustomNavGraph
+import com.galaxykud.notes.presentation.navigation.NavGraph
 import com.galaxykud.notes.presentation.ui.theme.NotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,23 +14,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NotesTheme {
-                EditNoteScreen(
-                    noteId = 5,
-                    onFinished = {
-                        Log.d("CreateNoteScreen", "Finished")
-                    }
-                )
+                NavGraph()
             }
-//            NotesTheme {
-//                NotesScreen(
-//                    onNoteClick = {
-//                        Log.d("MainActivity", "onNoteClick: $it")
-//                    },
-//                    onAddNoteClick = {
-//                        Log.d("MainActivity", "onAddNoteClick")
-//                    }
-//                )
-//            }
         }
     }
 }
